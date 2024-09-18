12e7155effd7d41c181d191504fcf7a19ba321f8
welintongomes <welintongm@gmail.com>
2024-09-18 13:08:31 -0300

    Update app.js

--- app.js
+++ app.js
@@ -18,27 +18,37 @@ que está no html e tem uma ID valor*/
 const h1_valor =  document.querySelector('#valor');
 
 document.querySelector('#btn_decremento').addEventListener('click', ()=>{
+    /*se o valor for == -10 interrompe e volta com o return*/
     if(valor == -10)return;
     h1_valor.textContent = --valor;
     /*chamando a funçao*/
-    set_valor()
+    set_color()
 })
 document.querySelector('#btn_incremento').addEventListener('click', ()=>{
+    /*se o valor for == 10 interrompe e volta com o return*/
     if(valor == 10)return;
     /*incrementando a variavel valor quando clica no botão*/
     h1_valor.textContent = ++valor;
     /*chamando a função, ela vai atualizando o valor na variavel valor*/
-    set_valor()
+    set_color()
 })
 /*cria uma função para modificar o valor de h1*/
-function set_valor(){
+function set_color(){
     
-    /*como o if e simples nao presisa colocar as açoes entre aspas se tivessemos mais açoes a ser executadas teria que ser entre {}*/
-    /*se o valor for exatamente igual a 0 tipo e valor vamos mudar o stylo color para branco*/
+    //como o if e simples nao presisa colocar as açoes entre aspas se tivessemos mais açoes a ser executadas teria que ser entre {}
+    //se o valor for exatamente igual a 0 tipo e valor vamos mudar o stylo color para branco
     if(valor === 0) h1_valor.style.color = "white"
-    /*senão for o de cima mais for este aqui quando valor for maior que 0 mude a cor para verde*/
+    //senão for o de cima mais for este aqui quando valor for maior que 0 mude a cor para verde
     else if(valor > 0) h1_valor.style.color = "green"
-    /*caso contrario se nao for nen 0 e nen maior que 0 será negativo entao mude a cor para vermelho*/
+    //caso contrario se nao for nen 0 e nen maior que 0 será negativo entao mude a cor para vermelho
     else h1_valor.style.color = "red"
 
+    // //fazendo a mesma coisa porém com um operador ternário
+    // // modifica o valor de h1_valor para receber a variavel valor
+    // h1_valor.textContent = valor;
+    
+    // // variavel color recebe cor branca se o valor for == 0, ou recebe vermelho se o valor for menor que 0
+    // // ou recebe verde caso o valor seja diferente de 0 ou caso seja maior que 0
+    // let color = valor == 0 ? 'white' : valor  < 0 ? 'red' : 'green'
+    // h1_valor.style.color = color;
 }
